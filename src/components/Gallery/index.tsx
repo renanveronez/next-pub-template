e081fd2f60a613f17d55e7687c52489305d1a465
Renan Veronez Drechsler <renanvz@outlook.com>
2022-01-31 16:14:55 -0300

    Gallery

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,13 +1,6 @@
 import { Gallery, Place } from './styles'
 import Faixatop from '../../assets/faixatop.svg';
 import Faixabottom from '../../assets/faixabottom.svg';
-import PicUm from '../../../public/FotoUm.jpg';
-import PicDois from '../../../public/FotoDois.jpg';
-import PicTres from '../../../public/FotoTres.jpg';
-import PicQuatro from '../../../public/FotoQuatro.jpg';
-import PicCinco from '../../../public/FotoCinco.jpg';
-import PicSeis from '../../../public/FotoSeis.jpg';
-import PicSete from '../../../public/FotoSete.jpg';
 
 function GalleryHome() {
   return (
@@ -23,31 +16,31 @@ function GalleryHome() {
       <Gallery>
         <div className='left' data-aos="fade-up">
           <div className='zoom'>
-            <img src={PicUm} alt="Bar" />
+            <img src="https://i.imgur.com/A7QsoVz.jpg" alt="Bar" />
           </div>
         </div>
         <div className='columnOne' data-aos="fade-up">
           <div className='zoom'>
-            <img src={PicDois} alt="Pic 1" />
+            <img src="https://i.imgur.com/BDZgE6i.jpg" alt="Pic 1" />
           </div>
           <div className='zoom'>
-            <img src={PicTres} alt="Pic 2" />
-          </div>
-        </div>
-        <div className='columnOne' data-aos="fade-up">
-          <div className='zoom'>
-            <img src={PicQuatro} alt="Pic 3" />
-          </div>
-          <div className='zoom'>
-            <img src={PicCinco} alt="Pic 4" />
+            <img src="https://i.imgur.com/Mc2BiSn.jpg" alt="Pic 2" />
           </div>
         </div>
         <div className='columnOne' data-aos="fade-up">
           <div className='zoom'>
-            <img src={PicSeis} alt="Pic 5" />
+            <img src="https://i.imgur.com/BDZgE6i.jpg" alt="Pic 3" />
           </div>
           <div className='zoom'>
-            <img src={PicSete} alt="Pic 6" />
+            <img src="https://i.imgur.com/Mc2BiSn.jpg" alt="Pic 4" />
+          </div>
+        </div>
+        <div className='columnOne' data-aos="fade-up">
+          <div className='zoom'>
+            <img src="https://i.imgur.com/BDZgE6i.jpg" alt="Pic 5" />
+          </div>
+          <div className='zoom'>
+            <img src="https://i.imgur.com/Mc2BiSn.jpg" alt="Pic 6" />
           </div>
         </div>
       </Gallery>
